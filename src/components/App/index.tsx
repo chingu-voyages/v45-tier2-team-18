6b1428cdeb5c7e5d06b892e2a00ba002c93aeba9
ItIsEternity <lavrovslava207@gmail.com>
2023-08-24 17:41:49 +0300

    antdesign basic theme setted up

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,25 +1,29 @@
 import React from 'react'
+import { ConfigProvider } from 'antd'
 import logo from '../../assets/logo.svg'
+import { ANT_THEME_TOKEN } from '../../data/constants'
 import './App.css'
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ConfigProvider theme={{ token: ANT_THEME_TOKEN }}>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ConfigProvider>
   )
 }
 
---
+++ src/data/constants.ts
@@ -0,0 +1,11 @@
+export const ANT_THEME_TOKEN = {
+  colorPrimary: '#2A8B8D',
+  colorLink: '#2A8B8D',
+  fontFamily: 'Manrope',
+  borderRadius: 12,
+  borderRadiusLG: 14,
+  borderRadiusOuter: 10,
+  borderRadiusSM: 10,
+  borderRadiusXS: 6,
+  colorBgContainer: '#1A1A1A99'
+}
