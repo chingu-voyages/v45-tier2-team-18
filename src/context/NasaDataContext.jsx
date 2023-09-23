e01aa8d0fd9aecccf87536c0863c3dcc7716944c
adhamElhabashy <adhammohamed8321@gmail.com>
2023-09-23 15:44:02 +0300

    modified the filter to make it accepts multiple filters

--- src/context/NasaDataContext.jsx
+++ src/context/NasaDataContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import axios from "axios";
 import papa from "papaparse";
-import useLocation from "../services/use-location";
+
 const NasaDataContext = createContext({
 	Meteorites: undefined,
 });
@@ -24,59 +24,53 @@ export function NasaDataContextProvider({ children }) {
 	}
 
 	function filteredData(searchParams) {
+		const filters = {};
+
 		if (searchParams.get("name")) {
-			return meteoritesData.filter(
-				(ele) => ele.name === searchParams.get("name")
-			);
-		} else if (searchParams.get("id")) {
-			return meteoritesData.filter((ele) => ele.id === searchParams.get("id"));
-		} else if (searchParams.get("recclass")) {
-			return meteoritesData.filter(
-				(ele) => ele.recclass === searchParams.get("recclass")
-			);
-		} else if (searchParams.get("mass (g)")) {
-			return meteoritesData.filter(
-				(ele) => ele["mass (g)"] === searchParams.get("mass (g)")
-			);
-		} else if (searchParams.get("year")) {
-			return meteoritesData.filter(
-				(ele) => ele.year == searchParams.get("year")
-			);
+			filters.name = searchParams.get("name");
 		}
-		//  else if (searchParams.get("country")) {
-		// 	return meteoritesData.filter((ele) => {
-		// 		let location = ele.GeoLocation.replace(/(\(|\))/g, "").split(", ");
-		// 		var config = {
-		// 			method: "get",
-		// 			url: `https://api.geoapify.com/v1/geocode/reverse?lat=${
-		// 				location[0]
-		// 			}&lon=${location[1]}&apiKey=${import.meta.env.VITE_GEOAPIFY_API_KEY}`,
-		// 			headers: {},
-		// 		};
-		// 		axios(config).then((res) =>
-		// 			setCountry(response.data.features[0].properties.country)
-		// 		);
-		// 		country === searchParams.get("country");
-		// 	});
-		// }
-		else if (searchParams.get("mass-g")) {
+
+		if (searchParams.get("id")) {
+			filters.id = searchParams.get("id");
+		}
+
+		if (searchParams.get("recclass")) {
+			filters.recclass = searchParams.get("recclass");
+		}
+
+		if (searchParams.get("mass (g)")) {
+			filters["mass (g)"] = searchParams.get("mass (g)");
+		}
+
+		if (searchParams.get("year")) {
+			filters.year = searchParams.get("year");
+		}
+
+		if (searchParams.get("mass-g")) {
 			const range = searchParams
 				.get("mass-g")
-				.replace(/,|-/g, "|")
-				.split("|")
-				.sort((a, b) => a - b);
-
-			setDataFiltering(
-				meteoritesData.filter(
-					(ele) =>
-						ele["mass (g)"] >= range[1] &&
-						ele["mass (g)"] <= range[range.length - 1]
-				)
-			);
-		} else {
-			return meteoritesData;
+				.replace(/,/g, "-")
+				.split("-")
+				.sort((a, b) => a - b)
+				.map(Number);
+			filters["mass (g)"] = {
+				min: range[1],
+				max: range[2],
+			};
 		}
-		return dataFiltering;
+
+		return meteoritesData.filter((meteorite) => {
+			return Object.keys(filters).every((key) => {
+				if (typeof filters[key] === "object") {
+					return (
+						meteorite[key] >= filters[key].min &&
+						meteorite[key] <= filters[key].max
+					);
+				} else {
+					return meteorite[key]?.toLowerCase() === filters[key]?.toLowerCase();
+				}
+			});
+		});
 	}
 
 	useEffect(() => {
--- src/sections/Tables/Search/Search.jsx
+++ src/sections/Tables/Search/Search.jsx
@@ -71,7 +71,6 @@ function Search() {
 						<MenuItem value={"recclass"}>Recclass</MenuItem>
 						<MenuItem value={"mass-g"}>Mass (g)</MenuItem>
 						<MenuItem value={"year"}>Year</MenuItem>
-						<MenuItem value={"country"}>Country</MenuItem>
 					</Select>
 				</FormControl>
 				<FormControl
@@ -90,26 +89,6 @@ function Search() {
 							}}
 						></TextField>
 					) : (
-						// <Select
-						// 	id="demo-simple-select-helper"
-						// 	value={massVal}
-						// 	onChange={(e) => {
-						// 		setMassVal(e.target.value);
-						// 	}}
-						// 	multiple
-						// 	sx={{ borderRadius: 0, width: "100%" }}
-						// >
-						// 	<MenuItem value="70m-50m">
-						// 		<em>70m - 50m</em>
-						// 	</MenuItem>
-						// 	<MenuItem value={"49m-20m"}>49m - 20m</MenuItem>
-						// 	<MenuItem value={"19m-1m"}>19m - 1m</MenuItem>
-						// 	<MenuItem value={"999k-500k"}>999k - 500k</MenuItem>
-						// 	<MenuItem value={"499k-100k"}>499k - 100k</MenuItem>
-						// 	<MenuItem value={"99k-10k"}>99k - 10k</MenuItem>
-						// 	<MenuItem value={"9k-1k"}>9k - 1k</MenuItem>
-						// 	<MenuItem value={"900-0"}>900 - 0</MenuItem>
-						// </Select>
 						<Select
 							labelId="demo-multiple-checkbox-label"
 							id="demo-multiple-checkbox"
